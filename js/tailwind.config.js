9fb9d33f00e5fc59fdc56c280189950998145a79
Alfan Zain <alfanzainkp@gmail.com>
2025-10-01 12:32:31 +0700

    feat: initial prototyping

---
+++ js/tailwind.config.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            fontFamily: {
+                'inter': ['Inter', 'sans-serif'],
+                'crimson': ['Crimson Pro', 'serif'],
+            },
+            colors: {
+                'brilliant-blue': '#0066FF',
+                'brilliant-green': '#2ACC58',
+                'brilliant-orange': '#FF6B35',
+                'brilliant-yellow': '#D8E82F',
+                'brilliant-purple': '#8B5CF6',
+                'brilliant-dark': '#1A1A1A',
+                'brilliant-grayish-yellow': '#FAFDEA',
+                'brilliant-gray': '#D1D1D1',
+            }
+        }
+    }
+}
